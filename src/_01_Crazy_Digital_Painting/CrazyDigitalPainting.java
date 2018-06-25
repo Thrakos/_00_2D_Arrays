138fd8d0140ed8686a494dbcd054d1f708bc2306
Rishi Khurana <league@WTS-IM-17.attlocal.net>
2018-06-24 17:25:57 -0700

    read the instruction of crazy painting digital thingy

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -3,15 +3,19 @@ package _01_Crazy_Digital_Painting;
 public class CrazyDigitalPainting {
 	//1. Create two final static integers for the width and height of the display.
 
+	final static int WIDTH = 1000;
+	final static int HEIGHT = 1000;
 	
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
 	
-	
+	int[][] colors = new int[WIDTH][HEIGHT];
 	
 	public CrazyDigitalPainting() {
 		//3. Open the crazy_digital_painting.png file and look at the image.
+		
+		// DOME!
 		
 		//4. Iterated through the 2D array and initialize each Color object
 		//   to a new color. The sample image was created using the following 
